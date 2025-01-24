9aeb76f030d6a1bafc13c1b83ad8b311765dcbe3
otahssiqueira <135395548+otahssiqueira@users.noreply.github.com>
2025-01-23 23:48:37 -0300

    v1 - variável js - EBAC
    Programa teste do curso de Javascript - EBAC. Meus primeiros passos.

---
+++ script.js
@@ -0,0 +1,12 @@
+function salvarValor() {
+
+input = document.getElementById("inputValue").value;
+
+console.log("VALOR CAPTURADO: " + input);
+
+document.getElementById("corpoSite").innerText = "O conteúdo digitado é: " + input;
+    
+}
+
+
+
